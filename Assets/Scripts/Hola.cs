f93cd903f2e5f8e720c495be5440b185027192ba
Rocio Ayala <eli19baer@gmail.com>
2022-08-29 14:50:46 -0400

    First commit

---
+++ Assets/Scripts/Hola.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hola : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hola en la consola");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
